e7e719474c4d99368d3de49ee9346fe03cd32348
ngryman <ngryman@gmail.com>
2017-04-25 14:18:22 +0200

    recycling ok

--- test/recycle.js
+++ test/recycle.js
@@ -1,9 +1,18 @@
 import test from 'ava'
-import { html, recycle } from '../'
+import { html, diff, recycle } from '../'
 
 test('recycle a vnode', t => {
   const oldVNode = html`<b>foo</b>`
   recycle(oldVNode)
   const newVNode = html`<b>foo</b>`
-  t.is(newVNode, oldVNode)
+  t.true(newVNode === oldVNode)
+})
+
+test('recycle patches', t => {
+  const oldBatch = diff(html`<b>foo</b>`, html`<i>foo</i>`)
+  const oldPatch = oldBatch[0]
+  recycle(oldBatch)
+  const newBatch = diff(html`<i>foo</i>`, html`<b>foo</b>`)
+  const newPatch = newBatch[1]
+  t.true(newPatch === oldPatch)
 })
