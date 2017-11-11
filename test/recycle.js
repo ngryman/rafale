be25c78ef904b4d2b3cb8828c8b6b22844d7bce3
ngryman <ngryman@gmail.com>
2017-11-11 13:24:03 +0100

    fix(test): recycle -> collect

--- test/recycle.js
+++ test/recycle.js
@@ -1,9 +1,9 @@
 import test from 'ava'
-import { html, diff, recycle } from '../index'
+import { html, diff, collect } from '../index'
 
 test('recycle a vnode', t => {
   const oldVNode = html`<b>foo</b>`
-  recycle(oldVNode)
+  collect(oldVNode)
   const newVNode = html`<b>foo</b>`
   t.true(newVNode === oldVNode)
 })
@@ -11,7 +11,7 @@ test('recycle a vnode', t => {
 test('recycle patches', t => {
   const oldBatch = diff(html`<b>foo</b>`, html`<i>foo</i>`)
   const oldPatch = oldBatch[0]
-  recycle(oldBatch)
+  collect(oldBatch)
   const newBatch = diff(html`<i>foo</i>`, html`<b>foo</b>`)
   const newPatch = newBatch[1]
   t.true(newPatch === oldPatch)
